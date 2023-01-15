27d417cc4970487cddb4c621485226357947f161
yamadamasashi <shisama94@gmail.com>
2023-01-16 00:19:58 +0900

    webpack によるコード分割

--- webpack-tutorial/source/index.js
+++ webpack-tutorial/source/index.js
@@ -1,13 +1,23 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { render } from "react-dom";
-
+import { getUsers } from "./common/usersAPI";
 import "./style.css"; // ローダーを設定しないと読み込まない
 import "./style.scss"; // scss用のローダーが必要
 import { hello } from "./sub";
 console.log( hello() )
 
+
+
 function App () {
     const [ state, setState ] = useState( "Click me" )
+    const [ datas, setDatas ] = useState( [] )
+
+    useEffect( () => {
+        getUsers().then( json => {
+            console.log( json )
+        } );
+    }, [] )
+
     return (
         <div>
             <button onClick={ () => setState( "clicked" ) }>{ state }</button>
