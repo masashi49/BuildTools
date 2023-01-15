8a85eee7ba738bef66683eea654b092f13b2fa94
yamadamasashi <shisama94@gmail.com>
2023-01-15 23:38:50 +0900

    reactをセットアップ完了

--- webpack-tutorial/source/index.js
+++ webpack-tutorial/source/index.js
@@ -1,4 +1,18 @@
+import React, { useState } from "react"
+import { render } from "react-dom";
+
 import "./style.css"; // ローダーを設定しないと読み込まない
 import "./style.scss"; // scss用のローダーが必要
 import { hello } from "./sub";
 console.log( hello() )
+
+function App () {
+    const [ state, setState ] = useState( "Click me" )
+    return (
+        <div>
+            <button onClick={ () => setState( "clicked" ) }>{ state }</button>
+        </div>
+    )
+}
+
+render( <App />, document.getElementById( "root" ) )
