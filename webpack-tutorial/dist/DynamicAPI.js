bcec49d1730dc0127a16c7f91d5f0ebf1528615e
yamadamasashi <shisama94@gmail.com>
2023-01-24 09:32:36 +0900

    webpackにて、cssのみ別ファイルに抜き出すplugin追加

---
+++ webpack-tutorial/dist/DynamicAPI.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebpack_tutorial=self.webpackChunkwebpack_tutorial||[]).push([[653],{3710:(t,e,r)=>{r.r(e),r.d(e,{getUsersDynamic:()=>n});var n=function(){return fetch("https://jsonplaceholder.typicode.com/users/").then((function(t){if(!t.ok)throw Error(t.statusText);return t.json()})).then((function(t){return t}))}}}]);
--- webpack-tutorial/webpack.config.js
+++ webpack-tutorial/webpack.config.js
@@ -1,5 +1,8 @@
 const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 const path = require( "path" );
+
+const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
     // エントリーポイント変更
@@ -26,21 +29,28 @@ module.exports = {
     module: {
         rules: [
             {
-                // cssをモジュールとして扱うために、css用のローダー
-                test: /\.css$/,
-                //use: [ "css-loader", "style-loader" ] // この並び順じゃないと動かない！ローダーは上から、左からロードされる。
-                use: [ {
-                    loader: "style-loader",
-                    options: { injectType: "lazySingletonStyleTag" },
-                }, "css-loader",
+                // cssをモジュールとして扱うために、css用のローダー 
+                test: /\.(sa|sc|c)ss$/i, // 正規表現でまとめるとよい
+                use: [
+                    // オブジェクトにする必要がある
+                    {
+                        loader: MiniCssExtractPlugin.loader, // cssを別ファイルにする
+                    },
+                    "css-loader",
                     "sass-loader" ]
-                ,
+                // buildが本番なら、cssをjsから抜き出すならこうする
+                //  use: [
+                //      devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+                //     "css-loader",
+                //     "sass-loader" ]
+
             },
+            // {
+            //     // scssをモジュールとして扱うために、css用のローダー
+            //     test: /\.scss$/,
+            //     use: [ "style-loader", "css-loader", "sass-loader" ]
+            // },
             {
-                // scssをモジュールとして扱うために、css用のローダー
-                test: /\.scss$/,
-                use: [ "style-loader", "css-loader", "sass-loader" ]
-            }, {
                 //最新のjs用のローダー 
                 // babelはjsコンパイラおよびトランスパイラ
                 test: /\.js$/,
@@ -56,5 +66,10 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin( {
             template: path.resolve( __dirname, "source", "index.html" )
-        } ) ],
+        } ),
+        new MiniCssExtractPlugin( {
+            filename: "style.css" // 出力する名前を入れないとエラー出る
+        } )
+
+    ],
 };
